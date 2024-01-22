6efce482255a6db574f6394b976bbb4f9ea8e5d4
Danny Mou <dannymou7426@hotmail.com>
2024-01-22 12:32:44 -0600

    First class

---
+++ firstclass/src/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    float b = 4.5;
+    char c = 'a';
+    string d = "Mou";
+    cout << "Hello World!" << endl;
+    cout << "Mou";
+    return 0;
+}
