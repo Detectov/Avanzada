4d5b2584945ef7ac65b8c4a63daf96bf8a29201c
Danny Mou <dannymou7426@hotmail.com>
2024-02-12 18:05:32 -0600

    Hanoi finished

--- hanoi/src/HanoiTowers.cpp
+++ hanoi/src/HanoiTowers.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+void HanoiTowers(int n, char from, char to, char aux){
+    if(n == 1 ){
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        return;
+    }
+    HanoiTowers(n-1, from, aux, to);
+    cout << "Move disk " << n << " from " << from << " to " << to << endl;
+    HanoiTowers(n-1, aux, to, from);
+}
+
 int main(){
-    
+ int numDisks;
+ cout << "Enter the number of disks: ";
+    cin >> numDisks;
+    HanoiTowers(numDisks, 'A', 'C', 'B');
 }
